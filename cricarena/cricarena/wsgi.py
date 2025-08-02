a74dc6c9c04df48a332a63a64310db94767ac2bd
Suraj31shah <suraj0631shah@gmail.com>
2025-08-02 19:27:00 +0530

    changes in wsgi.py

--- cricarena/cricarena/wsgi.py
+++ cricarena/cricarena/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import subprocess
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cricarena.settings')
-
 application = get_wsgi_application()
+subprocess.call(['python', 'manage.py', 'loaddata', 'backup.json'])

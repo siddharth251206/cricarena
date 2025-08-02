d02467b81694f7464d675e2daad3162be4bde240
Siddharth Sheth <shsheth2006@gmail.com>
2025-08-02 18:29:39 +0530

    internal database url changed in settings

--- cricarena/cricarena/settings.py
+++ cricarena/cricarena/settings.py
@@ -93,11 +93,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cricarena.wsgi.application'
 
 # Database
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600, 
+        ssl_require=True  
+    )
 }
 
 # Password validation

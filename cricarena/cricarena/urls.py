e155ff745e24aace2a81bf289ebb584bb4d2011a
SiddharthNITSURAT <shsheth2006@gmail.com>
2025-12-31 17:41:10 +0530

    added a ping for no sleep

--- cricarena/cricarena/urls.py
+++ cricarena/cricarena/urls.py
@@ -20,9 +20,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check),
     path('', entrance_view, name='entrance'),     # Default: /
     path('home/', home_view, name='h_index'),      # After clicking "Enter Arena"
     path('news/', include('news.urls')), # News page
